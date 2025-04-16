3c7ed9791433eaa2e6b77dcf72051508546cee3a
Jose Eduardo Monteiro <mds.josee@gmail.com>
2025-04-16 08:07:36 -0300

    sync

--- GlimpseOfLean/Exercises/02Iff.lean
+++ GlimpseOfLean/Exercises/02Iff.lean
@@ -35,7 +35,12 @@ prove one-by-one.
 -/
 
 example (a b : ℝ) (ha : 0 < a) (hb : 0 < b) : 0 < a^2 + b^2 := by {
-  sorry
+  apply add_pos
+  apply sq_pos_of_pos
+  exact ha
+
+  apply sq_pos_of_pos
+  exact hb
 }
 
 /-
@@ -60,7 +65,19 @@ example (a : ℝ) (ha : 0 < a) : 0 < (a^2)^2 := by {
 /- Now prove the same lemma as before using forwards reasoning. -/
 
 example (a b : ℝ) (ha : 0 < a) (hb : 0 < b) : 0 < a^2 + b^2 := by {
-  sorry
+  apply add_pos
+
+  have h2 : 0 < a^2 := by
+    apply sq_pos_of_pos
+    exact ha
+
+  exact h2
+
+  have h2b : 0 < b^2 := by
+    apply sq_pos_of_pos
+    exact hb
+
+  exact h2b
 }
 
 
@@ -79,7 +96,9 @@ example (a : ℝ) : a > 0 → b > 0 → a + b > 0 := by {
 /- Now prove the following simple statement in propositional logic.
 Note that `p → q → r` means `p → (q → r)`. -/
 example (p q r : Prop) : (p → q) → (p → q → r) → p → r := by {
-  sorry
+  intro h1 h2 h3
+  apply h2 h3
+  exact h1 h3
 }
 
 /- # Equivalences
@@ -110,7 +129,9 @@ Let's prove a variation
 -/
 
 example {a b : ℝ} (c : ℝ) : a + c ≤ b + c ↔ a ≤ b := by {
-  sorry
+  rw [← sub_nonneg]
+  ring
+  rw [sub_nonneg]
 }
 
 /-
@@ -148,7 +169,10 @@ example {a b : ℝ}  (ha : 0 ≤ a) : b ≤ a + b := by {
 /- Let's do a variant using `add_le_add_iff_left a : a + b ≤ a + c ↔ b ≤ c` instead. -/
 
 example (a b : ℝ) (hb : 0 ≤ b) : a ≤ a + b := by {
-  sorry
+  calc
+  a = a + 0 := by ring
+  _ ≤ a + b := by exact (add_le_add_iff_left a).2 hb
+
 }
 
 /-
@@ -179,7 +203,16 @@ example (a b : ℝ) : (a-b)*(a+b) = 0 ↔ a^2 = b^2 := by {
 /- You can try it yourself in this exercise. -/
 
 example (a b : ℝ) : a = b ↔ b - a = 0 := by {
-  sorry
+  constructor
+  intro h
+  rw [h]
+  ring
+
+  intro h
+  calc
+  a = b - (b -a) := by ring
+  _ = b - 0:= by rw [h]
+  _ = b := by ring
 }
 
 /-
